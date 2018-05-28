1321c6a31ee7300a45530242e89d18f90c542de1
shravani <shravani_hui@yahoo.com>
2018-05-28 10:33:15 +0530

    shravani added many changes-(28/05/18,10.32 AM)

---
+++ app/src/main/java/com/zuby/zubydriverdemo/Utils/CONSTANTS.java
@@ -0,0 +1,10 @@
+package com.zuby.zubydriverdemo.Utils;
+
+/**
+ * Created by citymapper-pc5 on 25/5/18.
+ */
+
+public class CONSTANTS
+{
+    public static final String server_key="AAAA0ITQ0sU:APA91bE-fSjA6wnrA27iQhXHADt-1UbLgRWER6Mw7Sa3OSNDS97IsMw3FfKuFXWvtXrsY5pMwaCziaabaqkZ5XaeCVDwCdeNFFf2aDwv8EbJkWQBzU8jee1nwTBcabLZ8KtfAfLARQbn";
+}
---
+++ app/src/main/java/com/zuby/zubydriverdemo/view/DashBoard/View/view/GrandObsession.java
@@ -0,0 +1,22 @@
+package com.zuby.zubydriverdemo.view.DashBoard.View.view;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+
+import com.zuby.zubydriverdemo.R;
+
+/**
+ * Created by citymapper-pc5 on 25/5/18.
+ */
+
+public class GrandObsession extends Activity
+{
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.grandobsession);
+    }
+}
